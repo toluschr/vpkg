a2e2019fc31672d6c5e092f301031919260e499c
toluschr <toluschr@protonmail.com>
2024-06-30 20:21:22 +0200

    vpkg: Set shlibs file to /var/lib/vpkg/shlibs

--- vpkg/defs.h
+++ vpkg/defs.h
@@ -3,6 +3,6 @@
 
 #define VPKG_TEMPDIR "/tmp/vpkg"
 #define VPKG_BINPKGS "/var/lib/vpkg"
-#define VPKG_XDEB_SHLIBS "/tmp/vpkg/shlibs"
+#define VPKG_XDEB_SHLIBS "/var/lib/vpkg/shlibs"
 
 #endif // VPKG_DEFS_H_
