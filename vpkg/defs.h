87ad5fa84a62280d2cd3d60fca43f1657e7e6f2e
toluschr <toluschr@protonmail.com>
2024-07-24 10:37:20 +0200

    vpkg-sync.toml, /etc/vpkg.ini -> /etc/vpkg-sync.toml, /etc/vpkg-install.ini

--- vpkg/defs.h
+++ vpkg/defs.h
@@ -1,7 +1,7 @@
 #ifndef VPKG_DEFS_H_
 #define VPKG_DEFS_H_
 
-#define VPKG_CONFIG_PATH "/etc/vpkg.ini"
+#define VPKG_CONFIG_PATH "/etc/vpkg-install.ini"
 #define VPKG_TEMPDIR "/tmp/vpkg"
 #define VPKG_BINPKGS "/var/lib/vpkg"
 #define VPKG_XDEB_SHLIBS "/var/lib/vpkg/shlibs"
