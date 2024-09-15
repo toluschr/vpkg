a5f76756fe2faf6834c84f1434245b4a4f90d4a5
toluschr <toluschr@protonmail.com>
2024-09-15 15:25:18 +0200

    Support multiple versions of package by using newest version

--- vpkg/config.cc
+++ vpkg/config.cc
@@ -2,6 +2,7 @@
 #include "vpkg/defs.h"
 
 #include <unistd.h>
+#include <string.h>
 #include <stdlib.h>
 #include <pwd.h>
 
@@ -10,11 +11,36 @@
 static int cb_ini_vpkg_config(const char *s_, size_t sl_, const char *k_, size_t kl_, const char *v_, size_t vl_, void *user_)
 {
     ::vpkg::config *user = static_cast<::vpkg::config *>(user_);
-    std::string_view section = s_ ? std::string_view{s_, sl_} : std::string_view{""};
-    std::string_view key = std::string_view{k_, kl_};
-    std::string_view value = std::string_view{v_, vl_};
 
-    auto [iterator, _] = user->insert(std::pair<std::string_view, vpkg::package>{section, {}});
+    auto section = s_ ? std::string_view{s_, sl_} : std::string_view{""};
+    auto key = std::string_view{k_, kl_};
+    auto value = std::string_view{v_, vl_};
+    auto version = std::string_view{};
+    const char *at;
+
+    if ((at = (const char *)memrchr(s_, '-', sl_))) {
+        int off = at - s_;
+        version = std::string_view{section.begin() + off + 1, section.end()};
+        section = section.substr(0, off);
+    }
+
+    auto [iterator, inserted] = user->insert(std::pair<std::string_view, vpkg::package>{section, {}});
+
+    if (!inserted && version.size() && iterator->second.version.size()) {
+        char this_pkgver[version.size() + 1] = {0};
+        char other_pkgver[iterator->second.version.size() + 1] = {0};
+
+        memcpy(this_pkgver, version.data(), version.size());
+        memcpy(other_pkgver, iterator->second.version.data(), iterator->second.version.size());
+
+        if (xbps_cmpver(this_pkgver, other_pkgver) < 0) {
+            return 0;
+        }
+    }
+
+    if (inserted && version.size()) {
+        iterator->second.version = version;
+    }
 
     if (section.size()) {
         if (key == "url") {
@@ -25,8 +51,6 @@ static int cb_ini_vpkg_config(const char *s_, size_t sl_, const char *k_, size_t
             iterator->second.name = value;
         } else if (key == "not_deps") {
             iterator->second.not_deps = value;
-        } else if (key == "version") {
-            iterator->second.version = value;
         } else if (key == "last_modified") {
             char *end;
             int eno = errno;
--- vpkg/config.hh
+++ vpkg/config.hh
@@ -11,12 +11,12 @@
 
 namespace vpkg {
 struct package {
-    std::string_view url;
-    std::string_view name;
-    std::string_view deps;
-    std::string_view version;
-    std::string_view not_deps;
-    time_t last_modified;
+    std::string_view url{};
+    std::string_view name{};
+    std::string_view deps{};
+    std::string_view version{};
+    std::string_view not_deps{};
+    time_t last_modified{0};
 };
 
 using config = typename std::map<std::string_view, vpkg::package>;
